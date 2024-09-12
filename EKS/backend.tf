e8603bfea37d49a752e727ad9f8cf0c22c00ad6f
opankaj-prog <omprakashpankaj2@gmail.com>
2024-09-12 22:32:04 +0530

    Update backend.tf

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mytodoappbucket"
-    key    = "eks/terraform.tfstate"
-    region = "us-east-2"
+    bucket = "myopbucket"
+    key    = "eks1/terraform.tfstate"
+    region = "us-east-1"
   }
 }
